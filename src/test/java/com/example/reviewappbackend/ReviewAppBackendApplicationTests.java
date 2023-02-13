37ac3ca69c057ad765f02a8d8761fbe2d1cb662b
radumbaciu <radumbaciu@gmail.com>
2023-02-13 19:14:09 +0200

    initial commit

---
+++ src/test/java/com/example/reviewappbackend/ReviewAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
